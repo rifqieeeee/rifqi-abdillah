f78ba1e10e90ab7cc42f97090f0d9af01b38df91
Rifqi Abdillah <>
2026-08-16 19:27:16 +0700

    new hero

---
+++ assets/js/home-slider.js
@@ -0,0 +1,18 @@
+new Swiper('.home-hero-slider', {
+  effect: 'fade',
+
+  fadeEffect: {
+    crossFade: true
+  },
+
+  loop: true,
+
+  speed: 1200,
+
+  autoplay: {
+    delay: 6500,
+    disableOnInteraction: false
+  },
+
+  allowTouchMove: false
+});
